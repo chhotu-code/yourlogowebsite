4f718e9f8380656f6385dcc7cf25a9f2b55e43f4
chhotu <chhotu456787@gmail.com>
2024-04-28 09:38:12 +0530

    kk

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,37 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/contact/Contact.jsx'
+import { RouterProvider } from 'react-router-dom'
+import Github, { githubdata } from './components/Github/Github.jsx'
+import User from './components/User/User.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Layout/>}>
+        <Route path='' element={<Home />} />
+        <Route path='home' element={<Home />} />
+        <Route path='about' element={<About/>} />
+        <Route path='contact' element={<Contact/>}/>
+        <Route path='user/:userid' element={<User/>}/>
+        <Route 
+        loader={githubdata}
+        path='/github'
+         element={<Github/>}
+         />
+  
+      </Route>
+    )
+  )
 
   return (
     <>
-    
+      <RouterProvider router={router}/>
     </>
   )
 }
